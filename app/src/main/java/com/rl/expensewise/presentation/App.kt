2ee706479a9cbb28a6a95fb5e420f025b1d11b4d
Ram Lakhan <ramlakhan.mobiledev@gmail.com>
2025-09-30 12:40:18 +0530

    setup app layout

--- app/src/main/java/com/rl/expensewise/presentation/App.kt
+++ app/src/main/java/com/rl/expensewise/presentation/App.kt
@@ -1,19 +1,95 @@
 package com.rl.expensewise.presentation
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.rl.expensewise.R
 import com.rl.expensewise.presentation.navigation.AppNavHost
+import com.rl.expensewise.presentation.navigation.Screen
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun App() {
 
     val navController: NavHostController = rememberNavController()
+    val currentBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = currentBackStackEntry?.destination?.route
 
-    Scaffold { innerPadding ->
+    val title = when(currentRoute) {
+        Screen.SignUpScreen.route -> stringResource(id = R.string.sign_up)
+        Screen.SignInScreen.route -> stringResource(id = R.string.sign_in)
+        Screen.HomeScreen.route -> stringResource(id = R.string.app_name)
+        else -> ""
+    }
+
+    Scaffold(
+        topBar = {
+            if (currentRoute != Screen.WelcomeScreen.route) {
+                TopAppBar(
+                    title = {
+                        Text(
+                            text = title,
+                            fontWeight = FontWeight.Bold
+                        )
+                    },
+                    navigationIcon = {
+                        if (currentRoute != Screen.HomeScreen.route) {
+                            IconButton(
+                                onClick = {
+                                    navController.popBackStack()
+                                }
+                            ) {
+                                Icon(
+                                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                                    contentDescription = stringResource(id = R.string.back)
+                                )
+                            }
+                        }
+                    },
+                    actions = {
+                        if (currentRoute == Screen.HomeScreen.route) {
+                            IconButton(
+                                onClick = {
+
+                                }
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.Person,
+                                    contentDescription = stringResource(id = R.string.profile),
+                                    modifier = Modifier
+                                        .size(48.dp)
+                                        .clip(CircleShape)
+                                        .background(MaterialTheme.colorScheme.surfaceVariant)
+                                        .padding(4.dp)
+                                )
+                            }
+                        }
+                    }
+                )
+            }
+        }
+    ) { innerPadding ->
         AppNavHost(
             modifier = Modifier.padding(innerPadding),
             navController = navController,
